e2ed3e77207edbc795746c7269baac05b4d93c6a
princeey1 <prince-david.madzou@polymtl.ca>
2025-02-16 10:57:32 -0500

    added same-tree solution

---
+++ Same-Tree/solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return this.traverseTree(p, q);
+    }
+
+    private boolean traverseTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        if (p.val != q.val)
+            return false;
+
+        boolean left = this.traverseTree(p.left, q.left);
+        boolean right = this.traverseTree(p.right, q.right);
+
+        return left && right;
+    }
+
+}
